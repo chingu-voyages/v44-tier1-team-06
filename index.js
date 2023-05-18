67d79e0d13a99aaff50a34aaa2440f333651044b
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-05-18 14:44:39 -0300

    Added timer functionality and style.

--- index.js
+++ index.js
@@ -55,3 +55,12 @@ submitButton.addEventListener("click", function () {
     clickedCell.removeEventListener("click", shade)
     );
 });
+
+//timer
+var timer = 60;
+
+var interval = setInterval(function() {
+    timer--;
+    $('.timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
